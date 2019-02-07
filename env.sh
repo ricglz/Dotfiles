1d65bfc60dd1f477ac6620b675399b374ca0cc91
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-02-07 15:03:39 -0600

    Update dotfiles

--- env.sh
+++ env.sh
@@ -3,6 +3,7 @@ export PATH="/usr/local/opt/ruby/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbi
 
 export PYTHONUSERBASE="${HOME}/myappenv"
 export PATH="/usr/local/lib/ruby/gems/2.5.0/gems/travis-1.8.9/bin:${HOME}/.npm-packages/bin:/usr/local/opt/python/libexec/bin:$PATH"
+export PATH="$HOME/.rbenv/bin:$PATH"
 
 # Specify default editor. Possible values: vim, nano, ed etc.
 export EDITOR=vim
@@ -10,21 +11,6 @@ export EDITOR=vim
 # Other exports
 export LANG=en_US.UTF-8
 export DEFAULT_USER=`whoami`
-
-# File search functions
-function f() { find . -iname "*$1*" ${@:2} }
-function r() { grep "$1" ${@:2} -R . }
-
-# Create a folder and move into it in one command
-function mkcd() { mkdir -p "$@" && cd "$_"; }
-
-# Add rbenv to bash so that it loads every time you open a terminal
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
-alias upDotfiles='cd && cp env.sh Dotfiles/ && cp .zshrc Dotfiles/ &&
-                  cp .bash_profile Dotfiles/ && cp .npmrc Dotfiles/ &&
-                  cp .vimrc Dotfiles/ && cp -R .vim/my_snippets Dotfiles/ &&
-                  Dotfiles && brew bundle dump --force'
 
 alias dcup='docker-compose up'
 alias dcdn='docker-compose down'
@@ -40,6 +26,5 @@ alias dcrdbs='docker-compose run --rm app rails db:seed'
 alias dcrgm='docker-compose run --rm app rails generate migration'
 
 alias dcrspec='docker-compose run --rm test rspec'
-
 alias bupgr='brew upgrade && brew cleanup'
 alias nupgr='npm update -g'
--- functions.sh
+++ functions.sh
@@ -8,30 +8,21 @@ function mkcd() { mkdir -p "$@" && cd "$_"; }
 # Update dotfiles
 function upDotfiles() {
   cd
-  files=(.gitconfig .npmrc .vimrc .zshrc env.sh functions.sh)
+  files=(.gitconfig .npmrc .vimrc .zshrc env.sh functions.sh .vim/my_snippets)
   for file in ${files[@]}; do
     echo "copying $file"
-    cp $file Dotfiles
+    if [[ -d $file ]] then
+      cp -R $file Dotfiles
+    else
+      cp $file Dotfiles
+    fi
   done
-  echo copying snippets
-  cp -R .vim/my_snippets Dotfiles
   cd Dotfiles
   echo doing brew bundle
   brew bundle dump --force
   echo pusing to git
-  # git add .
-  # git commit -m "Update dotfiles"
-  # git push -u origin master
+  git add .
+  git commit -m "Update dotfiles"
+  git push -u origin master
 }
 
-alias dcr='docker-compose run --rm app'
-
-alias dcrra='docker-compose run --rm app rails'
-
-alias dcrdbm='docker-compose run --rm app rails db:migrate'
-alias dcrdbr='docker-compose run --rm app rails db:reset'
-alias dcrdbs='docker-compose run --rm app rails db:seed'
-
-alias dcrgm='docker-compose run --rm app rails generate migration'
-
-alias dcrspec='docker-compose run --rm test rspec'
