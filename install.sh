faa8cce3ca72d0b23e307db3c6266a92c545f675
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-03-21 14:57:45 -0600

    Verify if homebrew is installed and install oh my zsh

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 # Install brew
-echo "Installing brew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Installing/Updating brew..."
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
 
 echo "Doing brew bundle..."
 brew bundle
+
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "Setting up symlinks"
 ./sl-setup.sh
