ba1c949ec417cb01b6e08608f36d899c27127360
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-03-21 14:43:10 -0600

    Add echos to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 # Install brew
+echo "Installing brew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo "Doing brew bundle..."
 brew bundle
 
+echo "Setting up symlinks"
 ./sl-setup.sh
