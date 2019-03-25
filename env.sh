9ce9643197300c7221186f6bf5eb6d728b7c0e34
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-03-25 15:02:44 -0600

    Update path in env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 # Add commonly used folders to $PATH
 export PATH="/usr/local/opt/ruby/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export PATH="${HOME}/.npm-packages/bin:/usr/local/opt/python/libexec/bin:$PATH"
-export PATH="/usr/local/lib/ruby/gems/2.6.0/bin:$PATH"
+export PATH="/usr/local/lib/ruby/gems/2.6.0/bin:$PATH:$HOME/.rvm/bin"
 export PATH="$HOME/.rbenv/bin:$PATH"
 
 export PYTHONUSERBASE="${HOME}/myappenv"
