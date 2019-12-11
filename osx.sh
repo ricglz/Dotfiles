5735fcc7adc6743f5abe1b5f2280d1e2ae80b9a7
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-12-10 23:17:27 -0600

    Update osx file

--- osx.sh
+++ osx.sh
@@ -76,9 +76,9 @@ defaults write com.apple.dashboard mcx-disabled -bool true
 # Change indexing order and disable some search results
 defaults write com.apple.spotlight orderedItems -array \
   '{"enabled" = 1;"name" = "APPLICATIONS' \
-  '{"enabled" = 1;"name" = "DOCUMENTS' \
-  '{"enabled" = 1;"name" = "DIRECTORIES' \
-  '{"enabled" = 1;"name" = "PDF";}' \
+  '{"enabled" = 0;"name" = "DOCUMENTS' \
+  '{"enabled" = 0;"name" = "DIRECTORIES' \
+  '{"enabled" = 0;"name" = "PDF";}' \
   '{"enabled" = 0;"name" = "SYSTEM_PREFS";}' \
   '{"enabled" = 0;"name" = "FONTS";}' \
   '{"enabled" = 0;"name" = "MESSAGES";}' \
