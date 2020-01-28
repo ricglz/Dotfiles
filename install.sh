d7401ad58b9c2320bdc029a0dd30eb72dd18e854
Ricardo González Castillo <rjgcastillo@hotmail.com>
2020-01-28 08:14:10 -0600

    Modified install.sh so that it asks if certain things want to be modified or not

--- install.sh
+++ install.sh
@@ -1,25 +1,37 @@
+answer_is_yes() {
+  [[ "$REPLY" =~ ^[Yy]$ ]] \
+    && return 0 \
+    || return 1
+  }
+
+ask_for_confirmation() {
+  print_question "$1 (y/n) "
+  read -n 1
+  printf "\n"
+}
+
+print_question() {
+  # Print output in yellow
+  printf "\e[0;33m  [?] $1\e[0m"
+}
+
 which -s brew
 if [[ $? != 0 ]] ; then
-    # Install Homebrew
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  echo "Installing homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-    brew update
+  echo "Updating homebrew"
+  brew update
 fi
-
-echo "Doing brew bundle..."
-brew bundle
 
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "Making directories"
-mkdir ~/.npm-packages
-
 echo "Setting up symlinks"
 ./sl-setup.sh
 
-echo "Installing poweline"
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+echo "Making directories"
+mkdir ~/.npm-packages
 
 echo "Adding extra folders for .vim"
 if [! -d "~/.vim/plugged/"]; then
@@ -30,11 +42,26 @@ if [! -d "~/.vim/undo/"]; then
   mkdir "~/.vim/undo/"
 fi
 
-echo "Installing npm global packages"
-./npm-g-installs.sh
+echo "Installing poweline"
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
-echo "Setting up configuration for osx"
-./osx.sh
+ask_for_confirmation "You want to install brew packages?"
+if answer_is_yes; then
+  echo "Doing brew bundle..."
+  brew bundle
+fi
+
+ask_for_confirmation "You want to install npm packages?"
+if answer_is_yes; then
+  echo "Installing npm global packages"
+  ./npm-g-installs.sh
+fi
+
+ask_for_confirmation "You want to setup osx configuration?"
+if answer_is_yes; then
+  echo "Setting up configuration for osx"
+  ./osx.sh
+fi
 
 echo "Finished installation"
 source ~/.zshrc
