94e00ca50dabb7c3b20af7e76d13d49753b2a007
Ricardo González Castillo <rjgcastillo@hotmail.com>
2020-02-20 07:21:25 -0600

    Fix some little things in osx.sh

--- osx.sh
+++ osx.sh
@@ -31,14 +31,13 @@ defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 # Stop iTunes from responding to the keyboard media keys
-launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
+launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2 > /dev/null
 
 ###############################################################################
 # Screen                                                                      #
 ###############################################################################
 
 # Save screenshots to the specific folder
-mkdir ~/Screenshots
 defaults write com.apple.screencapture location -string "${HOME}/Screenshots"
 
 # Disable shadow in screenshots
