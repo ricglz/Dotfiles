77a7ede3a5c4f25850aa1f0acf38b56b9a1dd260
Ricardo González Castillo <rjgcastillo@hotmail.com>
2020-03-10 19:35:32 -0600

    Minor change to latex installation script to avoid possible errors

--- latex_installation.zsh
+++ latex_installation.zsh
@@ -5,3 +5,4 @@ tlmgr update --self
 tlmgr install adjustbox babel-german background bidi collectbox csquotes everypage filehook footmisc \
   footnotebackref framed fvextra letltxmacro lm-math ly1 mdframed mweights needspace pagecolor sourcecodepro \
   sourcesanspro titling ucharcat ulem unicode-math upquote xecjk xurl zref
+tlmgr update --all
