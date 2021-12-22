f2c6b825429aa6d15143bf9348fba887dec03dca
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2021-12-22 11:42:56 -0600

    Start adding configuration

---
+++ nvim/lua/keybindings.lua
@@ -0,0 +1,24 @@
+require('nest').applyKeymaps {
+  {'<C-h>', '<C-w>h'},
+  {'<C-j>', '<C-w>j'},
+  {'<C-k>', '<C-w>k'},
+  {'<C-l>', '<C-w>l'},
+  {'<Tab>', '<cmd>bnext<CR>'},
+  {'<S-Tab>', '<cmd>bprevious<CR>'},
+  {'<C-p>', '<cmd>Telescope find_files<CR>'},
+  {'<C-a>', '<cmd>Telescope live_grep<CR>'},
+  {'Y', 'y$'},
+  {'J', 'mjJ`j'},
+  {'space', ":exe 'silent! normal! '.((foldclosed('.')>0)? 'zMzx' : 'zc')<CR>"},
+  {'-', '@@'},
+
+  {'<leader>', {
+    {'*', ":%s/\\<<C-r><C-w>\\>//g<Left><Left>"},
+    {'*', "hy:%s/\\V<C-r>h//g<Left><Left>", mode='v'},
+  }},
+
+  {mode='v', {
+    {'<', '<gv'},
+    {'>', '>gv'},
+  }},
+}
