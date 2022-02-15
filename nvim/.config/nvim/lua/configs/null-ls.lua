8168cda0a629bb375bc9b9e885642e1e3fcfd18b
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-02-14 21:29:55 -0600

    [WIP] Start adding interesting stuff for lsp

--- nvim/.config/nvim/lua/configs/null-ls.lua
+++ nvim/.config/nvim/lua/configs/null-ls.lua
@@ -1,14 +1,75 @@
 local null_ls = require("null-ls")
-null_ls.setup({
-  sources = {
-    -- JS yaml html markdown
-    null_ls.builtins.formatting.prettier,
-    -- Spell checking
-    null_ls.builtins.diagnostics.codespell.with({
-      args = { "--builtin", "clear,rare,code", "-" },
-    }),
-  },
-  on_attach = function()
-    vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
-  end,
-})
+
+local sources = {
+  null_ls.builtins.formatting.prettier,
+}
+
+local add_command = vim.api.nvim_add_user_command
+
+local no_lsp = function() return (#vim.lsp.buf_get_clients()) < 1 end
+
+local format_document = function()
+  if no_lsp() then
+    return
+  end
+
+  vim.lsp.buf.formatting_sync(nil, 1500)
+end
+
+local is_javascript = function()
+  local ft = vim.bo.filetype
+  return ft == 'javascript' or ft == 'typescript' or
+         ft == 'javascriptreact' or ft == 'typescriptreact'
+end
+
+local organize_imports = function()
+  if not is_javascript() or no_lsp() then
+    return
+  end
+
+  local params = {
+    command = '_typescript.organizeImports',
+    arguments = { vim.api.nvim_buf_get_name(0) },
+    title = '',
+  }
+  vim.lsp.buf_request_sync(vim.api.nvim_get_current_buf(), 'workspace/executeCommand', params, 1500)
+end
+
+local add_command_path = function()
+  add_command('Format', format_document, { desc = 'Format the document' })
+  if is_javascript() then
+    add_command('OrganizeImports', organize_imports, { desc = 'Organize imports via tsserver' })
+  end
+  vim.cmd([[
+    augroup fmt
+      autocmd BufWritePre * Format
+    augroup END
+  ]])
+end
+
+local other_path = function()
+  vim.cmd([[
+    augroup fmt
+      autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()
+    augroup END
+  ]])
+end
+
+local format = function(client)
+  if client.name ~= 'null-ls' then
+    return
+  end
+
+  if add_command == nil then
+    other_path()
+  else
+    add_command_path()
+  end
+end
+
+local on_attach = function(client)
+  vim.cmd('autocmd CursorHold * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor", border="rounded"})')
+  -- format(client)
+end
+
+null_ls.setup {sources = sources, on_attach = on_attach}
