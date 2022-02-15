ac08a8b1dc557238f7151fd7e95c5b34484c3041
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-02-14 21:29:36 -0600

    Remove vsnip because I was not using it

--- nvim/.config/nvim/lua/configs/nvim-cmp.lua
+++ nvim/.config/nvim/lua/configs/nvim-cmp.lua
@@ -9,38 +9,15 @@ end
 local cmp = require 'cmp'
 
 cmp.setup({
-  snippet = {
-    expand = function(args) vim.fn["vsnip#anonymous"](args.body) end
-  },
   mapping = {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-o>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({select = true}),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif vim.fn["vsnip#available"]() == 1 then
-        feedkey("<Plug>(vsnip-expand-or-jump)", "")
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-      end
-    end, {"i", "s"}),
-
-    ["<S-Tab>"] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-        feedkey("<Plug>(vsnip-jump-prev)", "")
-      end
-    end, {"i", "s"})
   },
   sources = {
     {name = 'nvim_lsp'},
-    {name = 'vsnip'},
     {name = 'buffer'},
     {name = 'omni'}
   }
