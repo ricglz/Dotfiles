9c0970b0008be3f24624c80b91bf33b75643d362
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-02-15 15:16:25 -0600

    Add some more formatting tools to null-ls

--- nvim/.config/nvim/lua/configs/null-ls.lua
+++ nvim/.config/nvim/lua/configs/null-ls.lua
@@ -2,7 +2,11 @@ local null_ls = require('null-ls')
 local on_attach = require('lsp.on_attach')
 
 local sources = {
+  null_ls.builtins.formatting.json_tool,
   null_ls.builtins.formatting.prettier,
+  null_ls.builtins.formatting.stylelint,
+  null_ls.builtins.formatting.trim_newlines,
+  null_ls.builtins.formatting.trim_whitespace,
 }
 
 null_ls.setup {sources = sources, on_attach = on_attach}
