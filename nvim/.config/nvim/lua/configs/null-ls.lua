f8e1e8cca5d4932fcbef1693a12a1daa00c00ca7
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-02-23 16:00:55 -0600

    Change to prettier_d and eslint_d

--- nvim/.config/nvim/lua/configs/null-ls.lua
+++ nvim/.config/nvim/lua/configs/null-ls.lua
@@ -3,7 +3,8 @@ local on_attach = require('lsp.on_attach')
 
 local sources = {
   null_ls.builtins.formatting.json_tool,
-  null_ls.builtins.formatting.prettier,
+  null_ls.builtins.formatting.prettierd,
+  null_ls.builtins.formatting.eslint_d,
   null_ls.builtins.formatting.stylelint,
   null_ls.builtins.formatting.trim_newlines,
   null_ls.builtins.formatting.trim_whitespace,
