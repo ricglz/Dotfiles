090d37a571a4c6a192a633296e5d252139a58f04
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-03-05 10:35:18 -0600

    Add pylint diagnostics

--- nvim/.config/nvim/lua/configs/null-ls.lua
+++ nvim/.config/nvim/lua/configs/null-ls.lua
@@ -2,12 +2,16 @@ local null_ls = require('null-ls')
 local on_attach = require('lsp.on_attach')
 
 local sources = {
+  -- formatters
   null_ls.builtins.formatting.json_tool,
   null_ls.builtins.formatting.prettierd,
   null_ls.builtins.formatting.eslint_d,
   null_ls.builtins.formatting.stylelint,
   null_ls.builtins.formatting.trim_newlines,
   null_ls.builtins.formatting.trim_whitespace,
+
+  -- diagnostics
+  null_ls.builtins.diagnostics.pylint
 }
 
 null_ls.setup {sources = sources, on_attach = on_attach}
