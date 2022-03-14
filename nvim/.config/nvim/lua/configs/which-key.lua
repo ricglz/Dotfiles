fa323bfb29b619c633385ae4bf73b2579021afd5
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-03-14 11:06:13 -0600

    Add missing registers for which-key (still lack for motions)

--- nvim/.config/nvim/lua/configs/which-key.lua
+++ nvim/.config/nvim/lua/configs/which-key.lua
@@ -1,13 +1,24 @@
 local wk = require("which-key")
 wk.setup()
+
 wk.register({
   ['*'] = {
     ':%s/\\<<C-r><C-w>\\>//g<Left><Left>',
     'Subtitutes all instances of word in buffer'
   },
-  d = 'Go to definition',
+  c = 'Perform code action',
+  D = 'See type definition',
   r = 'Rename symbol',
 }, { prefix = '<leader>'})
+
+wk.register({
+  b = 'Comment block',
+  c = 'Comment line',
+  d = 'Go to definition',
+  D = 'Go to declaration',
+  r = 'See references',
+  s = 'Sort',
+}, { prefix = 'g'})
 
 wk.register({
   ['*'] = {
