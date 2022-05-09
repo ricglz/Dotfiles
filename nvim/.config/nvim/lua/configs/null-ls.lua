da405a18752a59474ba9360b8b52742da7318c64
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-05-09 11:19:49 -0500

    Add rustfmt as formatter

--- nvim/.config/nvim/lua/configs/null-ls.lua
+++ nvim/.config/nvim/lua/configs/null-ls.lua
@@ -7,6 +7,7 @@ local sources = {
   null_ls.builtins.formatting.prettierd,
   null_ls.builtins.formatting.eslint_d,
   null_ls.builtins.formatting.stylelint,
+  null_ls.builtins.formatting.rustfmt,
   null_ls.builtins.formatting.trim_newlines,
   null_ls.builtins.formatting.trim_whitespace,
 
