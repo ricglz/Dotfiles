0ed13001500cfd45aaaa8452e5a084cdfb29199a
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-05-09 11:19:31 -0500

    nvim_add_user_command -> nvim_create_user_command

--- nvim/.config/nvim/lua/configs/fterm.lua
+++ nvim/.config/nvim/lua/configs/fterm.lua
@@ -1,5 +1,5 @@
 local fterm = require("FTerm")
-local add_command = vim.api.nvim_add_user_command
+local add_command = vim.api.nvim_create_user_command
 
 local gitui = fterm:new({cmd = "gitui"})
 add_command('Git', function() gitui:toggle() end, {})
